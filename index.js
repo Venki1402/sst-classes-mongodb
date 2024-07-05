4f876bd5d3f05a35baf9a9d917ddd413957f5fe0
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-07-05 11:17:03 +0530

    added a product to mongoDB

--- index.js
+++ index.js
@@ -1,24 +1,68 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-mongoose
-  .connect(
-    "mongodb+srv://venkiacademic:eTmTDWmNBZmJPoko@cluster0.bmzyfmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    console.log("Connected to MongoDB!");
-  })
-  .catch((err) => {
-    console.log("Failed: ", err);
-  });
-
 const app = express();
-const port = 3000;
+app.use(express.json());
 
+// test route
 app.get("/test", (req, res) => {
   res.send("Its Working...!");
 });
 
+// connect to mongodb
+mongoose
+.connect(
+  "mongodb+srv://venkiacademic:eTmTDWmNBZmJPoko@cluster0.bmzyfmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+.then(() => {
+  console.log("Connected to MongoDB!");
+})
+.catch((err) => {
+  console.log("Failed: ", err);
+});
+
+// product schema
+const productSchema = new mongoose.Schema({
+  product_name: {
+    type: String,
+    required: true,
+  },
+  product_price: {
+    type: String,
+    required: true,
+  },
+  isInStock: {
+    type: Boolean,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+// product model
+const productModel = mongoose.model("Product", productSchema);
+
+// create product
+app.post("/api/products", async (req, res) => {
+  const product = new productModel({
+    product_name: req.body.product_name,
+    product_price: req.body.product_price,
+    isInStock: req.body.isInStock,
+    category: req.body.category,
+  });
+  // console.log(product);
+  // return res.status(200).json(product);
+  const result = await product.save();
+  res.send(result);
+});
+
+
+
+
+
+// listen to port
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });

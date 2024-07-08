ba06c89e38cdc71354bbb7fb414c912f9f1c6583
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-07-08 11:55:13 +0530

    done

--- index.js
+++ index.js
@@ -1,126 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-
 const app = express();
+require("dotenv").config();
+
+const productRoutes = require("./routes/productRouter");
+const userRoutes = require("./routes/userRouter");
+
 app.use(express.json());
 
-
-// test route
-app.get("/test", (req, res) => {
-  res.send("Its Working...!");
-});
-
-// connect to mongodb
 mongoose
-  .connect(
-    process.env.MONGODB_URI,
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("Connected to MongoDB!");
+    console.log("MongoDb Connected");
   })
   .catch((err) => {
-    console.log("Failed: ", err);
+    console.log("MongoDb connection Failed", err);
   });
 
-// product schema
-const productSchema = new mongoose.Schema({
-  product_name: {
-    type: String,
-    required: true,
-  },
-  product_price: {
-    type: String,
-    required: true,
-  },
-  isInStock: {
-    type: Boolean,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-});
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
-// product model
-const productModel = mongoose.model("Product", productSchema);
-
-// create product
-app.post("/api/products", async (req, res) => {
-  const product = new productModel({
-    product_name: req.body.product_name,
-    product_price: req.body.product_price,
-    isInStock: req.body.isInStock,
-    category: req.body.category,
-  });
-  // console.log(product);
-  // return res.status(200).json(product);
-  const result = await product.save();
-  res.send(result);
-});
-
-// get all products
-// app.get("/api/products", async (req, res) => {
-//   const products = await productModel.find();
-//   res.send(products);
-// });
-// other way to get all products
-app.get("/api/products", async(req, res) => {
-  const products = await productModel.find({isInStock:true})
-  return res.send(products);
-});
-
-
-// update product
-app.put("/api/products/:id", async (req, res) => {
-  const product = await productModel.findByIdAndUpdate(
-    req.params.id,
-    {
-      product_name: req.body.product_name,
-      product_price: req.body.product_price,
-      isInStock: req.body.isInStock,
-      category: req.body.category,
-    },
-    { new: true }
-  );
-  if (!product) {
-    res.status(404).send("Product with given id is not found");
-  }
-  console.log(product);
-  res.send(product);
-});
-
-
-// get product by id
-app.get("/api/products/:id", async (req, res) => {
-  const product = await productModel.findById(req.params.id);
-  // if (!product) {
-    // res.status(404).send("Product with given id is not found");
-  // }
-  res.send(product);
-});
-
-// delete product
-// app.delete("/api/products/:id", async (req, res) => {
-//   const product = await productModel.findByIdAndRemove(req.params.id);
-//   if (!product) {
-//     res.status(404).send("Product with given id is not found");
-//   }
-//   res.send(product);
-// });
-// other way to delete product
-app.delete("/api/products/:id", async (req, res) => {
-  const product = await productModel.findByIdAndDelete(req.params.id);
-  if (!product) {
-    res.status(404).send("Product with given id is not found");
-  }
-  res.send(product);
-});
-
-// listen to port
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(8086, () => {
+  console.log("Server sarted at port http://localhost:8086");
 });
